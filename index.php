80ec594f19017d79fd43bddeb547989e3a987b56
Gautam4188 <168162276+Gautam4188@users.noreply.github.com>
2024-07-31 16:01:19 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <body>
 
-<h1>“People live their lives bound by what they accept as correct and true. That's how they define ‘reality’. But what does it mean to be ‘correct’ or ‘true’? Merely vague concepts... Their ‘reality’ may all be a mirage. Can we consider them to simply be living in their own world, shaped by their beliefs?"
+<h1>“this is gautam"
 </h1>
 
 <?php
